becd5413a8b1091ef52ea00c752766b3fd9b9bb7
Fine Archs <cicada1003@gmail.com>
2023-11-08 14:52:28 +0900

    Revert "Revert "MainArea""
    This reverts commit 713259952ef96e7e21bbad557d91c1c7a39b9f5b.

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+	<div id='wrapper'>
   <h1>
     AiScript (
     <MenuButton id="version" :options="menu" @select="onVersionSelect">{{
@@ -6,23 +7,16 @@
     }}</MenuButton>
     ) Playground
   </h1>
-  <Next v-if="version === 'next'"></Next>
-  <Develop v-if="version === 'develop'"></Develop>
-  <V0_16_0 v-if="version === '0.16.0'"></V0_16_0>
-  <V0_15_0 v-if="version === '0.15.0'"></V0_15_0>
-  <V0_14_1 v-if="version === '0.14.1'"></V0_14_1>
+	<div v-for='v in versions'>
+		<MainArea :ver='v' v-if='v==version'/>
+	</div>
+	</div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
+import MainArea, { versions, latest } from './MainArea.vue';
 import MenuButton from "@common/MenuButton.vue";
-import Next from "./versions/next/index.vue";
-import Develop from "./versions/develop/index.vue";
-import V0_16_0 from "./versions/0.16.0/index.vue";
-import V0_15_0 from "./versions/0.15.0/index.vue";
-import V0_14_1 from "./versions/0.14.1/index.vue";
-const versions = ["next", "develop", "0.16.0", "0.15.0", "0.14.1"] as const;
-const latest = "0.16.0";
 
 const version = ref(window.localStorage.getItem("version") ?? latest);
 const menu = Object.fromEntries(
@@ -62,8 +56,11 @@ body {
 </style>
 
 <style scoped>
+#wrapper {
+	padding: 16px;
+}
 h1 {
   font-size: 1.5em;
-  margin: 16px 16px 0 16px;
+  margin: 0 16px;
 }
 </style>
