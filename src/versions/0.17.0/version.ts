fa0b6c2f444eb0d3ef2098ce5c0a8244f08116ab
FineArchs <133759614+FineArchs@users.noreply.github.com>
2024-01-05 13:37:40 +0900

    aiscript version update (#54)
    * aiscript version update
    
    * re-npm i

---
+++ src/versions/0.17.0/version.ts
@@ -0,0 +1,18 @@
+import {
+  Interpreter,
+  Parser,
+  values,
+  utils,
+  Ast,
+} from "aiscript0_17_0";
+export { Interpreter, Parser, values, utils, Ast };
+export const version = "0.17.0";
+export const samples = {
+  ["Hello AiScript"]: '<: "Hello, AiScript!"',
+  FizzBazz: `for (let i, 100) {
+		<: if (i % 15 == 0) "FizzBuzz"
+			elif (i % 3 == 0) "Fizz"
+		elif (i % 5 == 0) "Buzz"
+		else i
+	}`,
+};
