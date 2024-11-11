e3b73a95fd6a367a08713b047a4b3230d343b7fa
FineArchs <cicada1003@gmail.com>
2024-11-11 10:40:29 +0900

    fix FizzBazz

--- src/versions/0.16.0/version.ts
+++ src/versions/0.16.0/version.ts
@@ -10,9 +10,9 @@ export const version = "0.16.0";
 export const samples = {
   ["Hello AiScript"]: '<: "Hello, AiScript!"',
   FizzBazz: `for (let i, 100) {
-		<: if (i % 15 == 0) "FizzBuzz"
-			elif (i % 3 == 0) "Fizz"
+	<: if (i % 15 == 0) "FizzBuzz"
+		elif (i % 3 == 0) "Fizz"
 		elif (i % 5 == 0) "Buzz"
 		else i
-	}`,
+}`,
 };
--- src/versions/develop/version.ts
+++ src/versions/develop/version.ts
@@ -4,9 +4,9 @@ export const version = "develop";
 export const samples = {
   ["Hello AiScript"]: '<: "Hello, AiScript!"',
   FizzBazz: `for (let i, 100) {
-		<: if (i % 15 == 0) "FizzBuzz"
-			elif (i % 3 == 0) "Fizz"
+	<: if (i % 15 == 0) "FizzBuzz"
+		elif (i % 3 == 0) "Fizz"
 		elif (i % 5 == 0) "Buzz"
 		else i
-	}`,
+}`,
 };
